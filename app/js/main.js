5efa819645a15045cad7ef144c92f22ecb025d04
abnovak <ashley@akta.com>
2014-12-26 12:27:14 -0600

    implemented about page with instagram feed.

--- app/js/main.js
+++ app/js/main.js
@@ -7,6 +7,25 @@ $(function() {
 
 		$(".carousel img").cover();
 
+		if ($("#instagram-container").length) {
+
+			var feed = new Instafeed({
+				get: 'tagged',
+				tagName: 'commonwealthcrew',
+				clientId: '00c5ad736e464e20bd242a1c3b410abe',
+				target: 'instagram-container',
+				sortBy: 'most-recent',
+				limit: 20,
+				template: '<li class="ig-image"><a  href="{{link}}"><img class="ig" src="{{image}}" /></a></li>',
+				resolution: 'low_resolution',
+				after: initScroller
+			});
+			feed.run();
+		}
 	});
+
+	function initScroller(){
+		$("#instagram-container").simplyScroll();
+	}
 
 });
